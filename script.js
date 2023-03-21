b14a4cca6952be2987a58d55146d148cf8e72074
Maharaja51442 <122883257+Maharaja51442@users.noreply.github.com>
2023-03-21 08:19:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("counts").innerHTML="10";
+    setTimeout(()=>{
+        document.getElementById("counts").innerHTML="9"
+        setTimeout(()=>{
+            document.getElementById("counts").innerHTML="8";
+            setTimeout(()=>{
+                document.getElementById("counts").innerHTML="7";
+                setTimeout(()=>{
+                    document.getElementById("counts").innerHTML="6";
+                    setTimeout(()=>{
+                        document.getElementById("counts").innerHTML="5";
+                        setTimeout(()=>{
+                            document.getElementById("counts").innerHTML="4";
+                            setTimeout(()=>{
+                                document.getElementById("counts").innerHTML="3";
+                                setTimeout(()=>{
+                                    document.getElementById("counts").innerHTML="10";
+                                    setTimeout(()=>{
+                                        document.getElementById("counts").innerHTML="10";
+                                        setTimeout(()=>{
+                                            document.getElementById("counts").innerHTML="Happy Independence Day";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},0)
